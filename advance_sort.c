b4bbe5a0d83edbf6ba51be84ef721bfa1677da2e
zanbyuu <fauzana@mhs.usk.ac.id>
2026-05-27 14:52:15 +0700

    functions.h, advance_sort.c

---
+++ advance_sort.c
@@ -0,0 +1,72 @@
+#include "functions.h"
+
+// Cetak n kata pertama dari array string
+void cetakArrayStr(char arr[][MAX_PANJANG_KATA], int n, int total) {
+    printf("  [ ");
+    for (int i = 0; i < n && i < total; i++) {
+        printf("\"%s\"", arr[i]);
+        if (i < n - 1 && i < total - 1) printf(", ");
+    }
+    if (total > n) printf(", ... (%d kata tersembunyi)", total - n);
+    printf(" ]\n");
+}
+
+// Shuffle array string dengan rand + swap
+void shuffleStr(char arr[][MAX_PANJANG_KATA], int n) {
+    char tmp[MAX_PANJANG_KATA];
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        strcpy(tmp,    arr[i]);
+        strcpy(arr[i], arr[j]);
+        strcpy(arr[j], tmp);
+    }
+}
+
+/* Membaca kata dari file .txt, satu kata per baris / per token */
+int bacaFile(char arr[][MAX_PANJANG_KATA], int maks) {
+    FILE *fp = fopen("words.txt", "r");
+    if (!fp) {
+        printf("[ERROR] File '%s' tidak ditemukan!\n", "words.txt");
+        printf("Pastikan file text ada di folder yang sama.\n\n");
+        return 0;
+    }
+    int n = 0;
+    while (n < maks && fscanf(fp, "%99s", arr[n]) == 1) n++;
+    fclose(fp);
+    return n;
+}
+
+
+//MERGE SORT
+
+//Fungsi bantu: gabungkan dua sub-array yang sudah terurut
+void merge(char arr[][MAX_PANJANG_KATA], int kiri, int tengah, int kanan) {
+    int n1 = tengah - kiri + 1;
+    int n2 = kanan - tengah;
+    char (*L)[MAX_PANJANG_KATA] = malloc((size_t)n1 * MAX_PANJANG_KATA);
+    char (*R)[MAX_PANJANG_KATA] = malloc((size_t)n2 * MAX_PANJANG_KATA);
+    if (!L || !R) { free(L); free(R); return; }
+    for (int i = 0; i < n1; i++) strcpy(L[i], arr[kiri + i]);
+    for (int j = 0; j < n2; j++) strcpy(R[j], arr[tengah + 1 + j]);
+    int i = 0, j = 0, k = kiri;
+    while (i < n1 && j < n2) {
+        if (strcmp(L[i], R[j]) <= 0)
+            strcpy(arr[k++], L[i++]);
+        else
+            strcpy(arr[k++], R[j++]);
+    }
+    while (i < n1) strcpy(arr[k++], L[i++]);
+    while (j < n2) strcpy(arr[k++], R[j++]);
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(char arr[][MAX_PANJANG_KATA], int kiri, int kanan) {
+    if (kiri < kanan) {
+        int tengah = kiri + (kanan - kiri) / 2; 
+        mergeSort(arr, kiri, tengah);
+        mergeSort(arr, tengah + 1, kanan);
+        merge(arr, kiri, tengah, kanan);
+    }
+}
